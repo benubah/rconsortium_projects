ac68aed05597ffc75411c1090a7ba73971e5c883
Ben <ben@rpowerlabs.org>
2019-05-24 13:05:33 +0100

    Update rladies.R

--- R/rladies.R
+++ R/rladies.R
@@ -1,9 +1,9 @@
 library(jsonlite)
 
 #to use a signed url for r-ladies
-all_rladies_groups <- fromJSON("https://api.meetup.com/find/groups?text=r-ladies&sig_id=262147300&radius=global&sig=f9ca12a1587739f82b5df22fe88f913f91254941");
+all_rladies_groups <- fromJSON("https://api.meetup.com/find/groups?text=r-ladies&sig_id=262147300&radius=global&sig=f9ca12a1587739f82b5df22fe88f913f91254941")
 selectedgroups <- data.frame(all_rladies_groups["city"],all_rladies_groups["country"])
 
 #all_rladies_groups %>% select("name","link","city", "localized_country_name", "timezone")
 
-write.csv(selectedgroups, "rladies.csv")
+write.csv(selectedgroups, "data/rladies.csv")
