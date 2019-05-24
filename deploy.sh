c53f583cf6761d065ecbeb5d4ec9ed6e66cb4b16
Ben <ben@rpowerlabs.org>
2019-05-24 23:03:13 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,22 +2,35 @@
 set -o errexit -o nounset
 BASE_REPO=$PWD
 
-commit_csvs() {
-  Rscript -e 'source("R/rladies.R")'
+setup_git() {
+Rscript -e 'source("R/rladies.R")'
 
   git config --global user.name "benubah"
   git config --global user.email "ben@rpowerlabs.org"
-  git config --global push.default simple
-  git remote add upstream "https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git"
-  git fetch upstream 2>err.txt
-
-
-  cp -fvr $BASE_REPO/data/* .
-  git add rladies.csv
-  git commit -a -m "#rstats ${TRAVIS_COMMIT_MESSAGE} (${TRAVIS_BUILD_NUMBER})"
-  git status
-  git push 2>err.txt
-  cd ..
 }
 
-commit_csvs
+commit_files() {
+  git checkout master
+  # Current month and year, e.g: May 2019
+  dateAndMonth=`date "+%b %Y"`
+  # Stage the modified files in dist/output
+  git add -f data/rladies.csv
+  # Create a new commit with a custom build message
+  # with "[skip ci]" to avoid a build loop
+  # and Travis build number for reference
+  git commit -m "Travis update: $dateAndMonth (Build $TRAVIS_BUILD_NUMBER)" -m "[skip ci]"
+}
+
+upload_files() {
+  # Remove existing "origin"
+  git remote rm origin
+  # Add new "origin" with access token in the git URL for authentication
+  git remote add origin https://benubah:${GITHUB_PAT}@github.com/benubah/${TRAVIS_REPO_SLUG}.git > /dev/null 2>&1
+  git push origin master --quiet
+}
+
+setup_git
+
+commit_files
+
+upload_files
