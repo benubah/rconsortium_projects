59caef6c00266320de632db76c3cc6056ae1b53c
Ben <ben@rpowerlabs.org>
2019-05-25 15:38:54 +0100

    Update rladies.R

--- R/rladies.R
+++ R/rladies.R
@@ -4,7 +4,7 @@ library(meetupr)
 get_rladies <- function() {
 #to use a signed url for r-ladies
 all_rladies_groups <- fromJSON("https://api.meetup.com/find/groups?text=r-ladies&sig_id=262147300&radius=global&sig=f9ca12a1587739f82b5df22fe88f913f91254941");
-#selectedgroups <- data.frame(all_rladies_groups["name"],all_rladies_groups["country"])
+selectedgroups <- data.frame(all_rladies_groups["name"],all_rladies_groups["country"])
 
 #all_rladies_groups %>% select("name","link","city", "localized_country_name", "timezone")
   
@@ -12,13 +12,13 @@ meetup_api_key <- Sys.getenv("MEETUP_KEY")
 #all_rladies_groups <- meetupr::find_groups(text = "r-ladies", api_key = meetup_api_key)
 
 # Cleanup
-rladies_groups <- all_rladies_groups[grep(pattern = "rladies|r-ladies|r ladies", 
-                                          x = all_rladies_groups$name,
-                                          ignore.case = TRUE), ]
+#rladies_groups <- all_rladies_groups[grep(pattern = "rladies|r-ladies|r ladies", 
+ #                                         x = all_rladies_groups$name,
+  #                                        ignore.case = TRUE), ]
 print("writing....")
   print(meetup_api_key)
   
-write.csv(rladies_groups, "data/rladies.csv")
+write.csv(selectedgroups, "data/rladies.csv")
    #write.csv(selectedgroups, "rladies.csv")
 }
 
