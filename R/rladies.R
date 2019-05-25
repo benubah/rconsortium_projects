839aa607608292d06b0c05e668d737dd1e3310ac
Ben <ben@rpowerlabs.org>
2019-05-25 15:19:31 +0100

    Update rladies.R

--- R/rladies.R
+++ R/rladies.R
@@ -1,9 +1,9 @@
-library(jsonlite)
+#library(jsonlite)
 library(meetupr)
 
 get_rladies <- function() {
 #to use a signed url for r-ladies
-#all_rladies_groups <- fromJSON("https://api.meetup.com/find/groups?text=r-ladies&sig_id=262147300&radius=global&sig=f9ca12a1587739f82b5df22fe88f913f91254941");
+all_rladies_groups <- fromJSON("https://api.meetup.com/find/groups?text=r-ladies&sig_id=262147300&radius=global&sig=f9ca12a1587739f82b5df22fe88f913f91254941");
 #selectedgroups <- data.frame(all_rladies_groups["name"],all_rladies_groups["country"])
 
 #all_rladies_groups %>% select("name","link","city", "localized_country_name", "timezone")
@@ -18,6 +18,7 @@ rladies_groups <- all_rladies_groups[grep(pattern = "rladies|r-ladies|r ladies",
 
 write.csv(rladies_groups, "data/rladies.csv")
   print("writing....")
+  print(meetup_api_key)
  
   #write.csv(selectedgroups, "rladies.csv")
 }
