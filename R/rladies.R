fd621a76567b4fc7de9b91f8f3259d6a12b3bd89
Ben <ben@rpowerlabs.org>
2019-05-25 15:33:05 +0100

    Update rladies.R

--- R/rladies.R
+++ R/rladies.R
@@ -3,13 +3,13 @@ library(meetupr)
 
 get_rladies <- function() {
 #to use a signed url for r-ladies
-#all_rladies_groups <- fromJSON("https://api.meetup.com/find/groups?text=r-ladies&sig_id=262147300&radius=global&sig=f9ca12a1587739f82b5df22fe88f913f91254941");
+all_rladies_groups <- fromJSON("https://api.meetup.com/find/groups?text=r-ladies&sig_id=262147300&radius=global&sig=f9ca12a1587739f82b5df22fe88f913f91254941");
 #selectedgroups <- data.frame(all_rladies_groups["name"],all_rladies_groups["country"])
 
 #all_rladies_groups %>% select("name","link","city", "localized_country_name", "timezone")
   
 meetup_api_key <- Sys.getenv("MEETUP_KEY")
-all_rladies_groups <- meetupr::find_groups(text = "r-ladies", api_key = meetup_api_key)
+#all_rladies_groups <- meetupr::find_groups(text = "r-ladies", api_key = meetup_api_key)
 
 # Cleanup
 rladies_groups <- all_rladies_groups[grep(pattern = "rladies|r-ladies|r ladies", 
