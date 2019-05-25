12c35894c8dedc22d4eb606c2b0751d280897eed
Ben <ben@rpowerlabs.org>
2019-05-25 15:57:06 +0100

    Update rladies.R

--- R/rladies.R
+++ R/rladies.R
@@ -12,9 +12,8 @@ meetup_api_key <- Sys.getenv("MEETUP_KEY")
 all_rladies_groups <- meetupr::find_groups(text = "r-ladies", api_key = meetup_api_key)
 
 # Cleanup
-rladies_groups <- all_rladies_groups[grep(pattern = "rladies|r-ladies|r ladies", 
- #                                         x = all_rladies_groups$name,
-  #                                        ignore.case = TRUE), ]
+rladies_groups <- all_rladies_groups[grep(pattern = "rladies|r-ladies|r ladies",  x = all_rladies_groups$name, ignore.case = TRUE), ]
+  
 print(rladies_groups)
   print(meetup_api_key)
   
